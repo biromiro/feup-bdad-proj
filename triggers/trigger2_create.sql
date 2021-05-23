9474babe7e875d8fd722fcba2366a7217bafd73f
Nuno Costa <n.r.t.c@hotmail.com>
2021-05-23 21:42:12 +0100

    Fix trigger 2 in respective file

--- triggers/trigger2_create.sql
+++ triggers/trigger2_create.sql
@@ -4,44 +4,29 @@ DROP TRIGGER IF EXISTS transportation_vaccine_storage_trigger;
 CREATE TRIGGER transportation_vaccine_storage_trigger
 AFTER
 INSERT ON transportation FOR EACH ROW BEGIN
-UPDATE vaccine_storage
-SET amount = (
-        (
-            SELECT amount
-            FROM vaccine_storage
-            WHERE vaccine_storage.infrastructure_id = NEW."to"
-                AND vaccine_storage.vaccine_id = NEW.vaccine_id
-        ) + NEW.amount
-    )
-WHERE NEW."to" = vaccine_storage.infrastructure_id
-    AND NEW."vaccine_id" = vaccine_storage.vaccine_id;
-UPDATE vaccine_storage
-SET amount = (
-        (
-            SELECT amount
-            FROM vaccine_storage
-            WHERE vaccine_storage.infrastructure_id = NEW."from"
-                AND vaccine_storage.vaccine_id = NEW.vaccine_id
-        ) - NEW.amount
-    )
-WHERE NEW."from" = vaccine_storage.infrastructure_id
-    AND NEW."vaccine_id" = vaccine_storage.vaccine_id;
-UPDATE infrastructure
-SET total_stored_vaccines = (
-        (
-            SELECT total_stored_vaccines
-            FROM infrastructure
-            WHERE infrastructure.id = NEW."to"
-        ) + NEW.amount
-    )
-WHERE NEW."to" = infrastructure.id;
-UPDATE infrastructure
-SET total_stored_vaccines = (
-        (
-            SELECT total_stored_vaccines
-            FROM infrastructure
-            WHERE infrastructure.id = NEW."from"
-        ) - NEW.amount
-    )
-WHERE NEW."from" = infrastructure.id;
+
+    INSERT INTO vaccine_storage(infrastructure_id, vaccine_id, amount) 
+    SELECT NEW."to", NEW.vaccine_id, 0
+    WHERE NOT EXISTS (
+        SELECT * FROM vaccine_storage WHERE NEW."to" = vaccine_storage.infrastructure_id AND NEW.vaccine_id = vaccine_storage.vaccine_id
+    );  
+
+    UPDATE vaccine_storage
+    SET amount = amount - NEW.amount
+    WHERE NEW."from" = vaccine_storage.infrastructure_id
+        AND NEW.vaccine_id = vaccine_storage.vaccine_id;
+
+    UPDATE vaccine_storage
+    SET amount = amount + NEW.amount
+    WHERE NEW."to" = vaccine_storage.infrastructure_id
+        AND NEW.vaccine_id = vaccine_storage.vaccine_id;
+
+    UPDATE infrastructure
+    SET total_stored_vaccines = total_stored_vaccines + NEW.amount
+    WHERE NEW."to" = infrastructure.id;
+
+    UPDATE infrastructure
+    SET total_stored_vaccines = total_stored_vaccines - NEW.amount
+    WHERE NEW."from" = infrastructure.id;
+
 END;
